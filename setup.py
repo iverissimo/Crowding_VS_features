f3aaa7374ca688284cf16c9bcf9ef6fa17fd01c7
iverissimo <verissimo.iness@gmail.com>
2022-07-26 11:39:27 +0200

    chaged names to avoid setuptools bug with underscore

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='CVSfeatures',
+    version='0.1',
+    url='https://github.com/iverissimo/Crowding_VS_features.git',
+    author='Ines S Verissimo',
+    author_email='s.verissimo.ines@gmail.com',
+    description='Description of my package',
+    packages=find_packages(),
+    install_requires=[],
+)
